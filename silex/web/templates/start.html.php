2ef2f115374d98007e078413bb1bb9231d1dec8e
nicolaikk <nkoukal@medionmail.com>
2016-01-31 16:44:32 +0100

    set up carousel

--- silex/web/templates/start.html.php
+++ silex/web/templates/start.html.php
@@ -1,8 +1,50 @@
 <?php $view->extend('layout.html.php') ?>
 
+
+             <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                 <!-- Indicators -->
+                 <ol class="carousel-indicators">
+                     <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                     <li data-target="#myCarousel" data-slide-to="1"></li>
+                     <li data-target="#myCarousel" data-slide-to="2"></li>
+                     <li data-target="#myCarousel" data-slide-to="3"></li>
+                 </ol>
+
+                 <!-- Wrapper for slides -->
+                 <div class="carousel-inner" role="listbox">
+                     <div class="item active">
+                         <img src="/images/skyscraper.jpg" alt="picture of city at night">
+                     </div>
+
+                     <div class="item">
+                         <img src="/images/servers.jpg" alt="picture of servers in a datacenter">
+                     </div>
+
+                     <div class="item">
+                         <img src="/images/aerialphoto.jpg" alt="picture of a field from above">
+                     </div>
+
+                 </div>
+
+                 <!-- Left and right controls -->
+                 <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                     <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                     <span class="sr-only">Previous</span>
+                 </a>
+                 <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                     <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                     <span class="sr-only">Next</span>
+                 </a>
+             </div>
+
+
+
+
 <div class="container-fluid">
     <div class="row">
+        <!--
         <img src="/images/skyscraper.jpg" alt=""/>
+        -->
         <div class="col-md-4" id="skyscraper">
 
         </div>

a7d8f51aff93f595de115bb82f4535622953ba7c
nicolaikk <nkoukal@medionmail.com>
2016-03-12 13:13:35 +0100

    - changed prefilling of the database

--- silex/sql/datadefinition.sql
+++ silex/sql/datadefinition.sql
@@ -1,3 +1,26 @@
 USE silex;
 
 -- CREATE YOUR TABLES HERE
+CREATE TABLE blog_post (
+  id         INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  author     VARCHAR(255),
+  title      VARCHAR(255),
+  text       TEXT,
+  created_at DATE
+);
+
+CREATE TABLE account (
+  id         INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  username   VARCHAR(255),
+  email      VARCHAR(255),
+  password   VARCHAR(255),
+  created_at DATE
+);
+
+CREATE TABLE comment (
+  id         INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  blog_post  INT,
+  title      VARCHAR(255),
+  text       TEXT,
+  created_at DATE
+);
--- silex/sql/datamanipulation.sql
+++ silex/sql/datamanipulation.sql
@@ -1,3 +1,6 @@
 USE silex;
 
 -- PREFILL YOUR TABLES HERE
+INSERT INTO blog_post (title, text, created_at) VALUES ('New Title', 'This is the content', CURRENT_DATE);
+INSERT INTO account (username, email, password, created_at) VALUE ('admin', 'admin@producify.io', 'admin', CURRENT_DATE);
+INSERT INTO comment (blog_post, title, text, created_at) VALUE (1, 'Testkommentar', 'Hallo Welt, das ist ein Test', CURRENT_DATE);

b11e5abb175cfe3a5640b3eede9a93e888f416dc
nicolaikk <nkoukal@medionmail.com>
2016-03-12 19:55:21 +0100

    - changed lable for input field

--- silex/web/templates/login.html.php
+++ silex/web/templates/login.html.php
@@ -7,8 +7,8 @@
             <div class="well">
                 <form role="form" action="/login" method="post">
                     <div class="form-group">
-                        <label for="email">Email oder Nutzername:</label>
-                        <input type="text" class="form-control" name="userInput" id="email">
+                        <label for="username">Email oder Nutzername:</label>
+                        <input type="text" class="form-control" name="userInput" id="username">
                     </div>
                     <div class="form-group">
                         <label for="pwd">Passwort:</label>

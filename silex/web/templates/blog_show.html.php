b84f1bfb7baeefc403864b93a6f35d79f16dda6f
nicolaikk <nkoukal@medionmail.com>
2016-03-14 22:54:42 +0100

    - changed design of blog view

--- silex/web/templates/blog_show.html.php
+++ silex/web/templates/blog_show.html.php
@@ -10,7 +10,7 @@
 <div class="container-fluid">
     <div class="row first">
         <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
+            <div class="panel panel-primary">
                 <div class="panel-heading">
                     Übersicht
                 </div>
